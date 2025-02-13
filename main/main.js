a45c56775011ebcecacf0d635acc10d3a0aa6cbc
Lisa <Lizaoma0@gmail.com>
2025-02-13 16:21:32 +0200

    js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,5 @@
+
+
 const numberUserNbElements = document.querySelectorAll('.number__user-nb');
 
 numberUserNbElements.forEach(function(numberUserNb) {
@@ -11,3 +13,24 @@ numberUserNbElements.forEach(function(numberUserNb) {
         numberUserNb.style.color = '';
     });
 });
+
+window.addEventListener('scroll', function() {
+    const background = document.querySelector('.backgroung-img');
+    const scrollPosition = window.scrollY; 
+    background.style.backgroundPosition = `330px ${250 + scrollPosition * 0.5}px`; 
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const text = document.querySelector(".features-text");
+ 
+    text.addEventListener("mousemove", function (e) {
+       const { left, top } = text.getBoundingClientRect();
+       const x = e.clientX - left;
+       const y = e.clientY - top;
+       text.style.setProperty("--x", `${x}px`);
+       text.style.setProperty("--y", `${y}px`);
+    });
+ });
+ 
