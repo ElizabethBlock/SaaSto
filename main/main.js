1d80a4f9a447c4af274969816d3e8c9bdda45154
Lisa <Lizaoma0@gmail.com>
2025-02-17 13:13:34 +0200

    js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,14 @@
+// window.addEventListener('load', function() {
+//     const background = document.querySelector('.backgroung-img');
+//     const scrollPosition = window.scrollY; 
+//     background.style.backgroundPosition = `10% ${270 + scrollPosition * 0.5}px`; 
+// });
+
+// window.addEventListener('scroll', function() {
+//     const background = document.querySelector('.backgroung-img');
+//     const scrollPosition = window.scrollY; 
+//     background.style.backgroundPosition = `10% ${270 + scrollPosition * 0.5}px`; 
+// });
 
 
 const numberUserNbElements = document.querySelectorAll('.number__user-nb');
@@ -14,23 +25,17 @@ numberUserNbElements.forEach(function(numberUserNb) {
     });
 });
 
-window.addEventListener('scroll', function() {
-    const background = document.querySelector('.backgroung-img');
-    const scrollPosition = window.scrollY; 
-    background.style.backgroundPosition = `330px ${250 + scrollPosition * 0.5}px`; 
+
+window.addEventListener("scroll", function () {
+    let scrollPos = window.scrollY;
+
+    document.querySelector(".item1").style.transform = `translateY(${scrollPos * 0.3}px)`;
+    document.querySelector(".item2").style.transform = `translateY(-${scrollPos * 0.2}px)`;
+    document.querySelector(".item3").style.transform = `translateY(${scrollPos * 0.4}px)`;
+    document.querySelector(".item5").style.transform = `translateY(${scrollPos * 0.4}px)`;
+    document.querySelector(".item6").style.transform = `translateY(${scrollPos * 0.2}px) rotate(${scrollPos * 0.1}deg)`;
+    document.querySelector(".item8").style.transform = `translateY(${scrollPos * 0.4}px)`;
 });
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const text = document.querySelector(".features-text");
- 
-    text.addEventListener("mousemove", function (e) {
-       const { left, top } = text.getBoundingClientRect();
-       const x = e.clientX - left;
-       const y = e.clientY - top;
-       text.style.setProperty("--x", `${x}px`);
-       text.style.setProperty("--y", `${y}px`);
-    });
- });
- 
